5027182468c9f36e870d5cfd723ada1d6841f977
masudfaraj <masudfaraj81@hotmail.com>
2024-09-14 12:28:32 +0200

    end of section 2

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using API.Data; 
+using API.EntitiesModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]  //    <=> api/users  vorsicht: [controller] mit klammern
+    public class UsersController(DataContext context) : ControllerBase // primary constructor
+    {
+        /*private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            // this.context = context;
+            _context = context;
+        }*/
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() { 
+            var users = await context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]  // api/users/2
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+            if (user == null) { return NotFound(); }
+            return user;
+        }
+    }
+}
---
+++ EntitiesModels/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.EntitiesModels
+{
+    public class AppUser
+    {
+        public int Id { get; set; } // mit Id Bennenung wird als PK gesehen
+        public required string Name { get; set; }
+       
+    }
+}
